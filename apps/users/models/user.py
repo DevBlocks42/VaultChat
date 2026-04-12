b1383a9b411e22d95423c23ff12047d27c50beaa
= <devblocks42@keemail.me>
2026-04-12 15:46:00 +0200

    Implémentation du modèle user.py + légère modification du MCD pour prendre en compte l'unicité de certaines propriétés de l'entité (username, email).

--- apps/users/models/user.py
+++ apps/users/models/user.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class Users(AbstractBaseUser):
+    email = models.EmailField(max_length=50, null=False, unique=True)
+    username = models.CharField(max_length=32, null=False, unique=True)
+    USERNAME_FIELD = "username"
+
