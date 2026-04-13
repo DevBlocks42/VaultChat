988a3b88d5a529a1d7e8a679243125c4b36cb631
= <devblocks42@keemail.me>
2026-04-13 15:12:51 +0200

    Frontend minimal pour l'inscription

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views.login_view import view as login_view
+from .views.register_view import view as register_view
 
 urlpatterns = [
     path('login', login_view , name='users-login'),
+    path('register', register_view , name='users-register'),
 ]
