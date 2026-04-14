8e0aaac3905b74a0157f8d47db20f645d52ceba1
= <devblocks42@keemail.me>
2026-04-14 19:11:52 +0200

    Correction erreur backend sur ajout d'utilisateur

--- apps/users/models/user.py
+++ apps/users/models/user.py
@@ -5,6 +5,7 @@ class User(AbstractBaseUser):
     email = models.EmailField(max_length=50, null=False, unique=True, verbose_name="adresse email")
     username = models.CharField(max_length=32, null=False, unique=True, verbose_name="nom d'utilisateur")
     USERNAME_FIELD = "username"
-
+    objects = models.Manager() 
+    
     class Meta:
         db_table = "users"
