5e45cd42ab4021763257a3f9cd5537f212c5a11f
= <devblocks42@keemail.me>
2026-04-14 02:35:30 +0200

    Ajout du modèle identity

---
+++ apps/users/models/identity.py
@@ -0,0 +1,9 @@
+from django.db import models
+from . import User
+
+class Identity(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, db_column="user_id", related_name="identity",)
+    signing_public_key = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = "identities"
