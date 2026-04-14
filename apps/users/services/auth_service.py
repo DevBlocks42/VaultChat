c1ddde556a6f2dc9b27cb3dd630bbb802ee1d861
= <devblocks42@keemail.me>
2026-04-14 12:52:51 +0200

    Commentaires normalisés dans identity_service + service orchestrateur auth_service.

---
+++ apps/users/services/auth_service.py
@@ -0,0 +1,8 @@
+from . import IdentityService, UserService
+
+
+class AuthService:
+
+    @staticmethod
+    def register_user(email : str, username : str, password : str, signing_public_key : str):
+        pass
--- apps/users/services/identity_service.py
+++ apps/users/services/identity_service.py
@@ -4,9 +4,27 @@ class IdentityService():
 
     @staticmethod
     def create_identity(user : User, signing_public_key : str):
+        """create_identity créer une nouvelle identité
+
+        Arguments:
+            user -- utilisateur associé
+            signing_public_key -- clef de signature Ed25519
+
+        Returns:
+            l'objet Identity fraîchement créé
+        """
         identity = Identity(user=user, signing_public_key=signing_public_key)
+        identity.save()
         return identity
 
-    def get_identity(id : int):
+    def read_identity(id : int):
+        """read_identity récupère une identité
+
+        Arguments:
+            id -- id d'identité
+
+        Returns:
+            l'objet Identity spécifié
+        """
         identity = Identity.objects.filter(id=id).first()
         return identity
