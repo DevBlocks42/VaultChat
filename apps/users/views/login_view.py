5b2a5c2fc3b166bc2802222b8a1fc8b88d6b5e7f
= <devblocks42@keemail.me>
2026-04-16 15:25:18 +0200

    Ajout protection anti replay + UI des messages d'erreurs/succès pour l'authentification

--- apps/users/views/login_view.py
+++ apps/users/views/login_view.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.conf import settings
 from ..services import AuthService, UserService
 from ..forms import LoginForm
+import time
 
 def view(request):
     form = LoginForm()
@@ -13,12 +15,16 @@ def view(request):
             password = form.cleaned_data["password"]
             signature = form.cleaned_data["signature"]
             challenge = request.session.get("auth_challenge")
+            issued_at = challenge.get('issued_at')
             nonce = challenge.get('nonce')
             user = authenticate(request, username=username, password=password)
-            if user is None:
-                messages.warning(request, "Nom d'utilisateur, mot de passe ou signature incorrect.")
+            if time.time() - issued_at > settings.CHALLENGE_TTL:
+                messages.error(request, "Le challenge a expiré.")
                 return redirect('users-login')
-            #UserService.read_user(username)
+                del request.session["auth_challenge"]
+            if user is None:
+                messages.error(request, "Nom d'utilisateur, mot de passe ou signature incorrect.")
+                return redirect('users-login')
             signing_public_key = user.identity.signing_public_key
             if AuthService.verify_challenge(nonce, signature, signing_public_key):
                 login(request, user)
@@ -26,7 +32,7 @@ def view(request):
                 messages.success(request, "Authentification réussie.")
                 return redirect('users-dashboard')
             else:
-                messages.warning(request, "Nom d'utilisateur, mot de passe ou signature incorrect.")
+                messages.error(request, "Nom d'utilisateur, mot de passe ou signature incorrect.")
                 return redirect('users-login')
     return render(request, "users/login.html", {
         'form': form
